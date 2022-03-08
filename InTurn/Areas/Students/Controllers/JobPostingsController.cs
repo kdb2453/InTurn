0c9e7a0cf9f16c2a9745dd2f8b3d1ac7764de6fc
Ty Simpson <ths2412@email.vccs.edu>
2022-03-08 01:24:24 -0500

    Show password button added to login/register views.

--- InTurn/Areas/Students/Controllers/JobPostingsController.cs
+++ InTurn/Areas/Students/Controllers/JobPostingsController.cs
@@ -13,7 +13,7 @@ namespace InTurn.Areas.Students.Controllers
 {
     public class JobPostingsController : Controller
     {
-        private InTurnContext db = new InTurnContext();
+        private InTurnEntities db = new InTurnEntities();
 
         // GET: Students/JobPostings
         public ActionResult Index()
