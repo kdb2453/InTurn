193177acd38ae7c0676e73977df4447ca2af2f60
Ty Simpson <ths2412@email.vccs.edu>
2022-03-08 14:01:21 -0500

    Added logo to home page and link to Business Website, Profile navigation added to Employer Area

--- InTurn/Controllers/AccountController.cs
+++ InTurn/Controllers/AccountController.cs
@@ -56,6 +56,8 @@ namespace InTurn.Controllers
             }
         }
 
+
+
         //
         // GET: /Account/Login
         [AllowAnonymous]
@@ -81,12 +83,14 @@ namespace InTurn.Controllers
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
             var result = await SignInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, shouldLockout: false);
-           
+        
+
             switch (result)
             {
                 case SignInStatus.Success:
                     var user = await UserManager.FindAsync(model.UserName, model.Password);
                     var roles = await UserManager.GetRolesAsync(user.Id);
+
                     if (roles.Contains("Student"))
                     {
                         return RedirectToAction("Index", "StudentHome", new { area = "Students" });
