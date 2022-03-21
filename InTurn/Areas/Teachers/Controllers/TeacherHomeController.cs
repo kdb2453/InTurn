bca63b6ea901751351e212b55c6ae9180bab6a48
Ty Simpson <ths2412@email.vccs.edu>
2022-03-21 14:14:23 -0400

    newpush

--- InTurn/Areas/Teachers/Controllers/TeacherHomeController.cs
+++ InTurn/Areas/Teachers/Controllers/TeacherHomeController.cs
@@ -10,7 +10,7 @@ using InTurn_Model;
 
 namespace InTurn.Areas.Teachers.Controllers
 {
-    [Authorize(Roles = "Admin,Employer")]
+    [Authorize(Roles = "Admin,Faculty")]
     public class TeacherHomeController : Controller
     {
         private InTurnEntities db = new InTurnEntities();
